0a7ff69ef830b4c40f2c201dcb35813eb27be5a9
Rand01ph <361212419@qq.com>
2015-09-01 16:08:36 +0800

    change email setting

--- contact-us/sendmail.php
+++ contact-us/sendmail.php
@@ -5,11 +5,11 @@ require_once "email.class.php";
 $smtpserver = "smtp.126.com";//SMTP服务器
 $smtpserverport =25;//SMTP服务器端口
 $smtpusermail = "tanyawei@126.com";//SMTP服务器的用户邮箱
-$smtpemailto = $_POST['your-email'];//发送给谁
+$smtpemailto = "xiedatelecom@gmail.com";//发送给谁
 $smtpuser = "tanyawei";//SMTP服务器的用户帐号
 $smtppass = "91tanyawei";//SMTP服务器的用户密码
 $mailtitle = $_POST['your-subject'];//邮件主题
-$mailcontent = "<p>".$_POST['your-name']."</p>"."<p>".$_POST['your-email']."</p>"."<p>".$_POST['your-message']."</p>";//邮件内容
+$mailcontent = "<p>Sender:".$_POST['your-name']."(".$_POST['your-email'].")</p>"."<p>".$_POST['your-message']."</p>";//邮件内容
 $mailtype = "HTML";//邮件格式（HTML/TXT）,TXT为文本邮件
 //************************ 配置信息 ****************************
 $smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.
